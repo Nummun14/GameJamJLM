97bfb1e1bda0826039f85ddd9fff0766f271fdb7
Noam tamir <114684941+The3rdNinja@users.noreply.github.com>
2025-01-02 17:47:02 +0200

    with gameoverscreen

---
+++ Assets/logicScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class logicScript : MonoBehaviour
+{
+    public GameObject GameOverScreen;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void restartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GameOver(){
+        GameOverScreen.SetActive(true);
+    }
+}
--- Assets/playerMovment.cs
+++ Assets/playerMovment.cs
@@ -4,24 +4,31 @@ public class playerMovment : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float speed = 10;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //public GameObject logic;
+    public logicScript logic;
+    public bool playerAlive = true;    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log(playerAlive);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow)) {
+        //Debug.Log(playerAlive);
+        if (Input.GetKey(KeyCode.RightArrow) && playerAlive) {
             myRigidbody.velocity = Vector2.right * speed;
         }
-        else if (Input.GetKey(KeyCode.LeftArrow)) {
+        else if (Input.GetKey(KeyCode.LeftArrow) && playerAlive) {
             myRigidbody.velocity = Vector2.left * speed;
         }
         else
         {
             myRigidbody.velocity = Vector2.zero;
         }
+    }
+
+    public void tryandtry() {
+        playerAlive = false;
     }
 }
