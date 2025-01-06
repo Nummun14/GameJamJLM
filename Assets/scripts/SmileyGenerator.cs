1c8b19cf144bf167f05999539f48ee0bdd91c0c1
Shai-51 <shaisagi02@gmail.com>
2025-01-06 17:39:02 +0200

    smily works only after an alert

--- Assets/scripts/SmileyGenerator.cs
+++ Assets/scripts/SmileyGenerator.cs
@@ -3,18 +3,16 @@ using UnityEngine;
 public class SmileyGenerator : MonoBehaviour
 {
     public Transform smileyPrefab;
-    public float spawnRate;
     private Transform lastSmiley;
      playerMovment player;
 
     void Start()
     {
-         player = GameObject.FindGameObjectWithTag("Player").GetComponent<playerMovment>();
-        InvokeRepeating("SpawnSmiley", 0f, spawnRate);
-    }
+         player = GameObject.FindGameObjectWithTag("Player").GetComponent<playerMovment>();    }
 
     public void SpawnSmiley()
     {
+
         if (lastSmiley != null)
         {
             Destroy(lastSmiley.gameObject);
@@ -25,8 +23,8 @@ public class SmileyGenerator : MonoBehaviour
         if (x == player.playerBody.position.x || y == player.playerBody.position.y){
             x = Random.Range(-5.5f, 6.0f);
         }
-
         lastSmiley = Instantiate(smileyPrefab, new Vector2(x, y), Quaternion.identity);
+
     }
 }
 
